c513574c34de7774d520ed90638ceec40f3f048f
fabianmolinab <molinafabian11b@gmail.com>
2020-06-30 18:00:16 -0500

    Reproductor Terminado

---
+++ reproductor.js
@@ -0,0 +1,33 @@
+var video = document.getElementById('video'),
+    btnPlay = document.getElementById('btnPlay'),
+    volumen = document.getElementById('volumen'),
+    icono = document.getElementById('icono'),
+    tiempo = document.getElementById('tiempo')
+    
+tiempo.setAttribute('max', video.duration)
+btnPlay.addEventListener('click',function(){
+    if (icono.className === 'fa fa-play'){
+        video.play()
+        var intervalo = setInterval(barraTiempo, 1)
+    } else {
+        video.pause()
+    }
+    //Alternar las clases del play y pause
+    icono.classList.toggle('fa-play')
+    icono.classList.toggle('fa-pause')
+})
+
+tiempo.addEventListener('change', function(){
+    video.currentTime = tiempo.value
+})
+
+function barraTiempo(){
+    tiempo.value = video.currentTime
+    if(video.ended){
+        icono.className = 'fa fa-play'
+    }
+}
+
+volumen.addEventListener('change', function(){
+    video.volume = volumen.value 
+})
